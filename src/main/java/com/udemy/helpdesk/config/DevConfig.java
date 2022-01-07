12ed2fdbac0ff412312f06cf4402b6d319f060c2
Robson Lopes <rob_lopes7@hotmail.com>
2022-01-07 11:48:52 -0300

    Perfil de Dev

--- src/main/java/com/udemy/helpdesk/config/DevConfig.java
+++ src/main/java/com/udemy/helpdesk/config/DevConfig.java
@@ -2,20 +2,26 @@ package com.udemy.helpdesk.config;
 
 import com.udemy.helpdesk.services.DBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
     @Autowired
-    DBService dbService;
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
 
     @Bean
     public void instaciaDB() {
-        this.dbService.instanciaDB();
+        if(value.equals("create")) {
+            this.dbService.instanciaDB();
+        }
     }
 
 }
