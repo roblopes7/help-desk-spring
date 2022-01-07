84f875de03a4eecc88227028e480628d8ae3c109
Robson Lopes <rob_lopes7@hotmail.com>
2022-01-07 17:12:23 -0300

    FindById para Chamado

---
+++ src/main/java/com/udemy/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,25 @@
+package com.udemy.helpdesk.resources;
+
+import com.udemy.helpdesk.domain.Chamado;
+import com.udemy.helpdesk.domain.dto.ChamadoDTO;
+import com.udemy.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    ChamadoService chamadoService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+        Chamado obj = chamadoService.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+}
---
+++ src/main/java/com/udemy/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.udemy.helpdesk.services;
+
+import com.udemy.helpdesk.domain.Chamado;
+import com.udemy.helpdesk.repositories.ChamadoRepository;
+import com.udemy.helpdesk.services.exceptions.ObjectNotFounException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> chamado = chamadoRepository.findById(id);
+        return chamado.orElseThrow(() -> new ObjectNotFounException("Chamado não encontrado! Id: " + id));
+    }
+}
