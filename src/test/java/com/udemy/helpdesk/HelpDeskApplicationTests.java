0b9c8eab44145f44db3b76497b320f3740f43b5c
Robson Lopes <rob_lopes7@hotmail.com>
2022-01-07 09:32:01 -0300

    first commit

---
+++ src/test/java/com/udemy/helpdesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
