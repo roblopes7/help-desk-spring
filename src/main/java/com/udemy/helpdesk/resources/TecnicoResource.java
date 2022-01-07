356ff0bf8ee918dfb389d84e04e1ff684653eb69
Robson Lopes <rob_lopes7@hotmail.com>
2022-01-07 14:14:35 -0300

    Padrao DTO para Tecnico

--- src/main/java/com/udemy/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/udemy/helpdesk/resources/TecnicoResource.java
@@ -1,6 +1,7 @@
 package com.udemy.helpdesk.resources;
 
 import com.udemy.helpdesk.domain.Tecnico;
+import com.udemy.helpdesk.domain.dto.TecnicoDTO;
 import com.udemy.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -18,9 +19,9 @@ public class TecnicoResource {
     TecnicoService tecnicoService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable("id") Integer id){
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable("id") Integer id){
         Tecnico obj = tecnicoService.findById(id);
-        return ResponseEntity.ok().body(obj);
+        return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
 }
