9cbcfa1140532d330bab7b708e41aa0bd899e575
Robson Lopes <rob_lopes7@hotmail.com>
2022-01-09 11:04:44 -0300

    Autenticacao

--- src/main/java/com/udemy/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/udemy/helpdesk/resources/TecnicoResource.java
@@ -4,8 +4,8 @@ import com.udemy.helpdesk.domain.Tecnico;
 import com.udemy.helpdesk.domain.dto.TecnicoDTO;
 import com.udemy.helpdesk.services.TecnicoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -34,19 +34,21 @@ public class TecnicoResource {
 
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TecnicoDTO> create(@RequestBody @Valid TecnicoDTO dto) {
         Tecnico tecnico =  tecnicoService.create(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(tecnico.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
-
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> update(@PathVariable Integer id, @RequestBody @Valid TecnicoDTO dto) {
         Tecnico obj = tecnicoService.update(id, dto);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<TecnicoDTO> delete(@PathVariable Integer id){
         tecnicoService.delete(id);
